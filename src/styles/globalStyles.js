51fa2b3152ebdd6db51c61915d64cfd7b30d1fc9
coderamos <diego.coderamos@gmail.com>
2022-03-31 18:51:26 -0300

    config fonts

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,7 +2,30 @@ import { createGlobalStyle } from 'styled-components';
 import normalize from './normalize';
 
 const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Montserrat Light'), local('Montserrat-Light'), url('/fonts/Montserrat-Light.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Montserrat Regular'), local('Montserrat-Regular'), url('/fonts/Montserrat-Regular.ttf') format('truetype');
+  }
+  
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Montserrat Bold'), local('Montserrat-Bold'), url('/fonts/Montserrat-Bold.ttf') format('truetype');
+  }
+  
   ${normalize};
+
 
   html {
     font-size: 50%;
@@ -13,6 +36,7 @@ const GlobalStyles = createGlobalStyle`
   }
   body {
     background-color: ${({ theme }) => theme.colors.white};
+    font-family: ${({ theme }) => theme.font.family};
   }
 `;
 
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,5 +4,9 @@ export default {
 
     white: '#FFFFFF',
     black: '#000000'
+  },
+  font: {
+    family:
+      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
   }
 };
