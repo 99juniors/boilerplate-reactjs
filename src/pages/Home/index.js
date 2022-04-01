ca7c5d57812e7b67371ab8550707fb163a7e3525
coderamos <diego.coderamos@gmail.com>
2022-04-01 15:27:31 -0300

    update Home page layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
+import { Button } from '../../components';
+
 import * as s from './styles';
 
 const HomePage = () => {
-  return <s.HomePageWrapper>homePage/</s.HomePageWrapper>;
+  return (
+    <s.HomePageWrapper>
+      <s.ContentWrapper>
+        HOME PAGE
+        <Button />
+      </s.ContentWrapper>
+    </s.HomePageWrapper>
+  );
 };
 
 export default HomePage;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -10,3 +10,14 @@ export const HomePageWrapper = styled.main`
     border: 4px solid ${theme.colors.primary};
   `}
 `;
+
+export const ContentWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  ${({ theme }) => css`
+    border: 4px solid ${theme.colors.black};
+    padding: 20px;
+  `}
+`;
