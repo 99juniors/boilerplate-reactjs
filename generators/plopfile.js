57271000b0ff5b75a6db771354b2112a4d65e55c
coderamos <diego.coderamos@gmail.com>
2022-04-11 21:42:44 -0300

    feat: create first component templates

---
+++ generators/plopfile.js
@@ -0,0 +1,29 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your COMPONENT name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.js',
+        templateFile: 'templates/component.js.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.js',
+        templateFile: 'templates/component-styles.js.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.js',
+        templateFile: 'templates/component-stories.js.hbs'
+      }
+    ]
+  });
+};
